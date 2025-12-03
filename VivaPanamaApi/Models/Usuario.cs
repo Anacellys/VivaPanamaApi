036ff8d8d4ab0c54f14f249cb6b0855ae8404d0f
DanelMorales <danelmoraless@hotmail.com>
2025-12-03 18:02:09 -0500

    Otra rama nueva

--- VivaPanamaApi/Models/Usuario.cs
+++ VivaPanamaApi/Models/Usuario.cs
@@ -1,11 +1,25 @@
-﻿namespace VivaPanamaApi.Models
+﻿using Microsoft.Win32;
+
+namespace VivaPanamaApi.Models
 {
     public class Usuario
     {
         public int IdUsuario { get; set; }
+
         public string Nombre { get; set; }
+
+        // Cedula o pasaporte del usuario (debe ser único)
         public string CedulaPasaporte { get; set; }
-        public int  Edad { get; set; }
+
+        public int Edad { get; set; }
+
+        // Email único
         public string Email { get; set; }
+
+        // Relación 1 a 1s con Preferencia
+        public Preferencias Preferencia { get; set; }
+
+        // Solo lectura (no vas a hacer CRUD de estos de momento)
+        public List<Calificacion> Calificaciones { get; set; }
     }
 }
