f8ad0f414371f675df5e22962a25cd6136f9968f
Danel Morales <danelmoraless@hotmail.com>
2025-12-02 19:12:06 -0500

    Algunas cosas...

--- VivaPanamaApi/Program.cs
+++ VivaPanamaApi/Program.cs
@@ -1,25 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // este de aqui crea el constructor del proyecto
+// aqui se lee el appsettings.json, se cargan variables, servicios,etc. 
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(); //esto añade controladores
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); //Swagger es el UI donde pruebas el API
 
-var app = builder.Build();
+var app = builder.Build(); // Aqui se crea la aplicacion lista para atender peticiones HTTP 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) // Si estas en modo desarrollo activa Swagger, activa el explorador de la API
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); //Fuerza que las peticiones sean HTTPS
 
-app.UseAuthorization();
+app.UseAuthorization(); // Habilita la autorización (aunque aún no tienes auth configurado)
 
-app.MapControllers();
+app.MapControllers(); //Conecta todas las clases Controller.
 
-app.Run();
+app.Run(); //Arranca el servidor 
