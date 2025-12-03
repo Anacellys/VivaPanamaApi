75be8811f105437d254b353f28b7357fde665911
Anacellys <anacelisboniche04@gmail.com>
2025-12-03 01:19:43 -0500

    Se estan Agregando los combios de la versiones de Api

---
+++ VivaPanamaApi/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using VivaPanamaApi.Models;
+
+namespace VivaPanamaApi.Contexts
+{
+    public class ApplicationDbContext : DbContext   // ← Modificación 1: heredar de DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // ← Modificación 2: agregar DbSet para la tabla calificacion
+        public DbSet<Calificacion> Calificaciones { get; set; }
+    }
+}
---
+++ VivaPanamaApi/Models/Calificacion.cs
@@ -0,0 +1,20 @@
+﻿namespace VivaPanamaApi.Models
+{
+    public class Calificacion
+    {
+        public int Id_Calificacion { get; set; }
+
+        public int Id_Usuario { get; set; }
+
+        public int Id_Lugar { get; set; }
+
+        public int Puntuacion { get; set; }
+
+        public string? Comentario { get; set; }
+
+        public DateTime? Fecha { get; set; }
+
+        public Usuario? Usuario { get; set; }
+        public Lugar? Lugar { get; set; }
+    }
+}
